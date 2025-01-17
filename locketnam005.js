2fb82f8c126d6fdcd3c25c01755e984e0714080a
Hoàng nam Music <153541300+caovannam2ka5@users.noreply.github.com>
2025-01-17 14:01:29 +0700

    Update locketnam005.js

--- locketnam005.js
+++ locketnam005.js
@@ -1,11 +1,11 @@
-// ========= ID ========= //
-const mapping = {
+// ========== Mã số ========= //
+ánh xạ hằng số = {
     '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-    'Locket': ['Gold']
+    'Mềm': ['Vàng']
   };
-  // =========   Phần cố định  ========= // 
-  // =========  @Hoàng Nam 005 ========= // 
-  // ========= Facebook : Cao Văn Nam (v) ==========//
-  //========== Bản Này Đang Bán Với Giá 2tr ===========//
-  //========== Cấm Thằng Nào Share  chỉnh tháng trước ngày sau nhá ==============//
-  var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Bản Này Độc Quyển Của Hoàng Nam 2005";var nam005={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-10T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2025-01-01T01:04:18Z",purchase_date:"2025-01-01T01:04:17Z",store:"app_store"},Hoangnam005={grace_period_expires_date:null,purchase_date:"2025-01-01T01:04:17Z",product_identifier:"com.nam005.premium.yearly",expires_date:"2099-12-10T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(Hoangnam005.product_identifier=s,obj.subscriber.subscriptions[s]=nam005):obj.subscriber.subscriptions["com.nam005.premium.yearly"]=nam005,obj.subscriber.entitlements[e]=Hoangnam005}else obj.subscriber.subscriptions["com.nam005.premium.yearly"]=nam005,obj.subscriber.entitlements.pro=Hoangnam005;$done({body:JSON.stringify(obj)});
+  // ========= Cố định phần cứng ========== //
+  // ========= @Hoàng Nam 005 ========== //
+  // ========= Facebook : Cao Văn Nam (v) ===========//
+  //========== Bản Này Đang Bán Với Giá 2tr ============//
+  //========== Cấm Thằng Bé Share chỉnh tháng trước ngày sau nhá ===============//
+  var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Bản Này Độc Quyển Của Hoàng Nam 2005";var nam005={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-10T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2025-01-17T01 :04:18Z",ngày_mua_hàng:"2025-01-17T01:04:17Z",cửa_hàng:"cửa_hàng_ứng_dụng"},Hoangnam005={grace_period_expires_date:null,ngày_mua_hàng:"2025-01-17T01:04:17Z",mã_sản_phẩm:"com.nam005.premium.yearly",ngày_hết_hết:"2099-12-10T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(Hoangnam005.product_identifier=s,obj.subscriber.subscriptions[s]=nam005):obj.subscriber.subscriptions["com.nam005.premium.yearly"]=nam005,obj.subscriber.entitlements[e]=Hoangnam005}nếu không thì obj.subscriber.subscriptions["com.nam005.premium.yearly"]=nam005,obj.subscriber.entitlements.pro=Hoangnam005;$done({body:JSON.stringify(obj)});
